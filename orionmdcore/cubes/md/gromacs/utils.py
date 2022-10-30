9071899226e6acb49c8b909718103b046528d21e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 10:49:39 +0000

    Adding tarfile member sanitization to extractall()

--- orionmdcore/cubes/md/gromacs/utils.py
+++ orionmdcore/cubes/md/gromacs/utils.py
@@ -190,7 +190,26 @@ def gmx_run(opt):
             file_tar = download_file(file_restart_id, tar_restart_fn)
 
             with tarfile.open(file_tar) as tar:
-                tar.extractall(path=outdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=outdir)
 
             restart_fn_prev = os.path.join(
                 outdir,
--- orionmdcore/mdrecord/mdrecord.py
+++ orionmdcore/mdrecord/mdrecord.py
@@ -100,7 +100,26 @@ def stage_system(f):
             )
 
             with tarfile.open(fn) as tar:
-                tar.extractall(path=dir_stage)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=dir_stage)
 
             mdrec.processed[stage_name] = dir_stage
 
@@ -1141,7 +1160,26 @@ class MDDataRecord(object):
         )
 
         with tarfile.open(trj_tar) as tar:
-            tar.extractall(path=traj_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=traj_dir)
 
         trj_field = stage.get_field(Fields.trajectory.get_name())
         trj_meta = trj_field.get_meta()
